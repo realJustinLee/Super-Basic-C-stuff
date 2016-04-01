f6ab46ea624f4fc0f5e95f6b2ace6f6ee4a39f71
李欣 <lixin@lixin-computer.com>
2016-04-01 13:03:35 +0800

    Lixin Commit

--- 第二周作业/02.c
+++ 第二周作业/02.c
@@ -4,10 +4,10 @@ int main(void)
     int a,b,c,d,e;
     printf("Enter a number:");
     scanf("%d",&a);
-    b=(a/1000+9)/10;
-    c=((a/100)%10)+9)/10;
-    d=((a/10)%10+9)/10;
-    e=(a%10+9)/10;
+    b=(a/1000+9)%10;
+    c=((a/100)%10+9)%10;
+    d=((a/10)%10+9)%10;
+    e=(a%10+9)%10;
     printf("The encrypted number is %d%d%d%d",d,e,b,c);
     return 0;
 }
