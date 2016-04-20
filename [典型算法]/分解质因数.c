4f1255bcc01b33811d4ec26efc9eb1562b000b95
李欣 <lixin@lixin-computer.com>
2016-04-20 16:35:49 +0800

    Lixin Commit

---
+++ [典型算法]/分解质因数.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main(void){
+    int n,i,j;
+    printf("n:");
+    scanf("%d",&n);
+    for ( i = 2; i <=sqrt(n); i++) {
+        if (n%i==0){
+            n=n/i;
+            printf("%d*",i--);
+        }
+    }
+    printf("%d",n);
+    return 0;
+}
---
+++ [典型算法]/统计字符.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+	char s;
+	int a=0,b=0,c=0,d=0;
+	printf("Input 10 characters: ");
+	while((s=getchar())!='\n') {
+		if((s<='z'&&s>='a')||(s<'Z'&&s>'A')) {
+			a++;
+		}
+		else if(s==' ')
+			b++;
+		else if(s<58&&s>47)
+			c++;
+		else d++;
+	}
+	printf("letter=%d,blank=%d,digit=%d,other=%d",a,b,c,d);
+	return 0;
+}
