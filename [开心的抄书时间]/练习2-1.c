e29beb4720c44d4bee45d24947b4414b3f54a5b2
李欣 <lixin@lixin-computer.com>
2016-04-23 15:55:29 +0800

    Lixin Commit

---
+++ [开心的抄书时间]/练习2-1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void){
+    printf("Programming in C fun!");
+    return 0;
+}
---
+++ [开心的抄书时间]/练习2-2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+    printf("Programming is fun. And programing in C is even more fun!\n");
+    printf("Programming is fun.\nAnd programing in C is even more fun!\n");
+    printf("Programming is fun.\n");
+    printf("And programing in C is even more fun!\n");
+    return 0;
+}
