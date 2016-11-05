e3c5ba31cf17c6233f1e82261c5677932a1e4b20
李欣 <lixin@lixin-computer.com>
2016-11-06 00:42:39 +0800

    Lixin Commit

---
+++ CPP_PP/strquote.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string version1(const string s1, const string &s2) {
+    string temp;
+    temp = s2 + s1 + s2;
+    return temp;
+}
+
+const string &version2(string &s1, const string &s2) {
+    s1 = s2 + s1 + s2;
+    return s1;
+}
+
+const string &version3(string &s1, const string &s2) {
+    string temp;
+    temp = s2 + s1 + s2;
+    return temp;
+}
+
+int main() {
+    string input;
+    string copy;
+    string result;
+
+    cout << "Enter a string: ";
+    getline(cin, input);
+    copy = input;
+    cout << "Your string as entered: " << input << endl;
+    result = version1(input, "***");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+
+    result = version2(input, "###");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+
+    cout << "Resetting original string.\n";
+    input = copy;
+    result = version3(input, "@@@");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
+
+    return 0;
+}
